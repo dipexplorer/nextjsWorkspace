9e1d7a002ceba23fe23c7ea5f61643f0d4e702da
dip <dipexplorerid23@gmail.com>
2026-03-08 10:55:20 +0530

    reset password implementation

--- next_auth_app/app/profile/[id]/page.tsx
+++ next_auth_app/app/profile/[id]/page.tsx
@@ -1,12 +1,18 @@
 export default async function UserProfile({ params }: any) {
-  const { id } = await params;
-  return (
-    <div>
-      <h1>User Profile</h1>
-      {/* Render user profile details here */}
-      <p>User ID: {id}</p>
-      {/* Add more user details as needed */}
-      <p>More user details...</p>
-    </div>
-  );
+    const { id } = await params;
+    return (
+        <div>
+            <h1>User Profile</h1>
+            {/* Render user profile details here */}
+            <p>User ID: {id}</p>
+            {/* Add more user details as needed */}
+            <p>Update your profile </p>
+            {/* reset password */}
+            <button className="">Reset Password</button>
+            <p>
+                An email is forwared of reseting your password, view the email
+                and click on the reset link...
+            </p>
+        </div>
+    );
 }
